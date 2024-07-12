66b127ad74350b2f295a333276f80ce7241327e1
Janhavi Rai <janhavirai2000@gmail.com>
2024-07-12 05:47:09 +0530

    server established

---
+++ server.js
@@ -0,0 +1,17 @@
+
+const  express =require('express');
+//rest object
+const app = express();
+//rest api
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to ecommerce app</h1>");
+});
+
+//PORT
+const PORT =  8080;
+
+//run listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${PORT}`);
+});
