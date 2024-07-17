023aaa2c5a85d777a8e7141af7c45f86daced9ea
Janhavi Rai <janhavirai2000@gmail.com>
2024-07-17 19:55:02 +0530

    adding middlewares

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,16 @@
 import express from 'express';
-import { registerController ,loginController} from "../controllers/authController.js";
+import { registerController ,loginController,testController} from "../controllers/authController.js";
 const router= express.Router();
 
+import { isAdmin,requireSignIn } from './../middlewares/authMiddlewares';
 
 //routing
 //REGISTER || METHOD POST
 router.post("/register", registerController);
 //LOGIN || POST
 router.post("/login", loginController);
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
+
 
 export default router;
