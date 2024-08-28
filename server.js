ed18fb737adc250e08bec4e24fabaebc4601335e
Janhavi Rai <janhavirai2000@gmail.com>
2024-08-28 15:28:10 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import colors from 'colors';
-import morgan from 'morgan';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoute.js';
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
+
+//configure env
 dotenv.config();
 
 //databse config
@@ -13,12 +18,14 @@ connectDB();
 const app = express();
 
 //middelwares
-
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
 //rest api
 app.get("/", (req, res) => {
@@ -31,6 +38,7 @@ const PORT = process.env.PORT || 8080;
 //run listen
 app.listen(PORT, () => {
   console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
   );
 });
