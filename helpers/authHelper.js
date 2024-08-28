4998388a16e818003f672650b361d63ca0cc0d51
Janhavi Rai <janhavirai2000@gmail.com>
2024-08-28 15:24:53 +0530

    Add files via upload

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -1,4 +1,5 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
+
 export const hashPassword = async (password) => {
   try {
     const saltRounds = 10;
@@ -8,6 +9,7 @@ export const hashPassword = async (password) => {
     console.log(error);
   }
 };
+
 export const comparePassword = async (password, hashedPassword) => {
   return bcrypt.compare(password, hashedPassword);
 };
